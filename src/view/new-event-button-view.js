c9d76013787d77fa6ad59921b099de4e4ebee525
Daria Egarmina <d.egarmina@gmail.com>
2025-05-19 16:30:17 +0300

    Делает заготовку обработчика события клика по кнопке New event, WIP

--- src/view/new-event-button-view.js
+++ src/view/new-event-button-view.js
@@ -4,7 +4,23 @@ const createNewEventButtonTemplate = () =>
   '<button class="trip-main__event-add-btn  btn  btn--big  btn--yellow" type="button">New event</button>';
 
 export default class NewEventButtonView extends AbstractView {
+  #handleNewEventButtonClick = null;
+
+  constructor({ onNewEventButtonClick } = {}) {
+    super();
+
+    this.#handleNewEventButtonClick = onNewEventButtonClick;
+
+    this.element.addEventListener('click', this.#newEventButtonClickHandler);
+  }
+
   get template() {
     return createNewEventButtonTemplate();
   }
+
+  #newEventButtonClickHandler = (evt) => {
+    evt.preventDefault();
+    console.log('click');
+    // this.#handleNewEventButtonClick();
+  };
 }
