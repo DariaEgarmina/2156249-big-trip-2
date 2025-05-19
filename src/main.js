32f3aabf277ae18714c5d5dd22c12ee742afe33e
Daria Egarmina <d.egarmina@gmail.com>
2025-05-19 15:37:41 +0300

    Создает view для кнопки New event и отрисовывет её

--- src/main.js
+++ src/main.js
@@ -1,9 +1,11 @@
+import NewEventButtonView from './view/new-event-button-view.js';
 import FiltersView from './view/filters-view.js';
 import TripEventsPresenter from './presenter/trip-events-presenter.js';
 import { render } from './framework/render.js';
 import PointsModel from './model/points-model.js';
 import { generateFilter } from './mock/filter.js';
 
+const headerControlsContainer = document.querySelector('.trip-main');
 const filtersContainer = document.querySelector('.trip-controls__filters');
 const tripEventsContainer = document.querySelector('.trip-events');
 
@@ -16,6 +18,7 @@ const tripEventsPresenter = new TripEventsPresenter({
 
 const filters = generateFilter(pointsModel.points);
 
+render (new NewEventButtonView(), headerControlsContainer);
 render(new FiltersView({filters}), filtersContainer);
 
 tripEventsPresenter.init();
